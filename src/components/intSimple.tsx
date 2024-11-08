32815eddd7d068f9af20097123d35a70008e2ed9
andersonruizr2 <andersonruizr2@gmail.com>
2024-11-08 01:12:02 +0000

    update file

--- src/components/intSimple.tsx
+++ src/components/intSimple.tsx
@@ -27,48 +27,54 @@ export default function InterestCalculator() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col items-center gap-4">
-      <label>
-        Capital
-        <input
-          type="number"
-          value={capital}
-          onChange={(e) => setCapital(e.target.value)}
-          className="border pl-2 border-neutral-600 rounded-lg"
-          required
-        />
-      </label>
-      <label>
-        Tasa de interés anual (%)
-        <input
-          type="number"
-          value={interesAnual}
-          onChange={(e) => setInteresAnual(e.target.value)}
-          className="border pl-2 border-neutral-600 rounded-lg"
-          required
-        />
-      </label>
-      <label>
-        Tiempo en años
-        <input
-          type="number"
-          value={tiempo}
-          onChange={(e) => setTiempo(e.target.value)}
-          className="border pl-2 border-neutral-600 rounded-lg"
-          required
-        />
-      </label>
-      <button
-        type="submit"
-        className="bg-red-600 rounded-md text-white w-32 h-10"
-      >
-        Calcular
-      </button>
-      <div>
-        <p>Interés simple: {resultado}</p>
-        <p>Total: {resultadoAcumulado}</p>
+    <>
+      <div className="w-5/6 md:w-1/2 px-2 py-2 flex flex-col justify-center items-center border rounded-lg shadow-lg bg-white">
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col items-center gap-4"
+        >
+          <label>
+            Capital
+            <input
+              type="number"
+              value={capital}
+              onChange={(e) => setCapital(e.target.value)}
+              className="border pl-2 border-neutral-600 rounded-lg"
+              required
+            />
+          </label>
+          <label>
+            Tasa de interés anual (%)
+            <input
+              type="number"
+              value={interesAnual}
+              onChange={(e) => setInteresAnual(e.target.value)}
+              className="border pl-2 border-neutral-600 rounded-lg"
+              required
+            />
+          </label>
+          <label>
+            Tiempo en años
+            <input
+              type="number"
+              value={tiempo}
+              onChange={(e) => setTiempo(e.target.value)}
+              className="border pl-2 border-neutral-600 rounded-lg"
+              required
+            />
+          </label>
+          <button
+            type="submit"
+            className="bg-red-600 rounded-md text-white w-32 h-10"
+          >
+            Calcular
+          </button>
+          <div>
+            <p>Interés simple: {resultado}</p>
+            <p>Total: {resultadoAcumulado}</p>
+          </div>
+        </form>
       </div>
-    </form>
+    </>
   );
-};
-
+}
